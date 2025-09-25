ac7c550d3088f9a8b50e3ada82c915e3086e94cf
mahadi609im <maha609im@gmail.com>
2025-09-25 11:59:14 +0600

    update

--- src/Components/ToggleBtns.jsx
+++ src/Components/ToggleBtns.jsx
@@ -4,7 +4,7 @@ import Container from './Container';
 const ToggleBtns = ({ toggleBtnClick, setToggleBtnClick, btns }) => {
   return (
     <Container>
-      <div className="text-right ">
+      <div className="">
         {btns.map((btn, idx) => {
           return (
             <button
@@ -13,8 +13,8 @@ const ToggleBtns = ({ toggleBtnClick, setToggleBtnClick, btns }) => {
                 setToggleBtnClick(btn);
                 console.log('Clicked:', btn);
               }}
-              className={`${btn == btns[0] && 'rounded-l-lg'} toggle-btn ${
-                btn == btns[btns.length - 1] && 'rounded-r-lg'
+              className={`${btn == btns[0] && 'md:rounded-l-lg'} toggle-btn ${
+                btn == btns[btns.length - 1] && 'md:rounded-r-lg'
               } ${toggleBtnClick === btn && '!bg-slate-800 !text-purple-600'}`}
             >
               {btn}
